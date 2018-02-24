17db0b31ff706e0c1013b66e5fef5d3c1a1649f6
Alex Baratti <alexsmbaratti@gmail.com>
2018-02-24 14:04:31 -0700

    Adds GetMarioMaker.js

---
+++ commands/GetMarioMaker.js
@@ -0,0 +1,11 @@
+const Command = require('./Command.js');
+
+// Example code:XXXX-XXXX-XXXX-XXXX
+
+class GetMarioMaker extends Command {
+  constructor(msg) {
+    super(msg);
+  }
+}
+
+module.exports = GetMarioMaker;
