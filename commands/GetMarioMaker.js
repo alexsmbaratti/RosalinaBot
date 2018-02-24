a6f7b0058dacc9c77e1f79e87bec62085e603928
Alex Baratti <alexsmbaratti@gmail.com>
2018-02-24 14:06:03 -0700

    Adds example site and fixes typo in comments

--- commands/GetMarioMaker.js
+++ commands/GetMarioMaker.js
@@ -1,6 +1,7 @@
 const Command = require('./Command.js');
 
-// Example code:XXXX-XXXX-XXXX-XXXX
+// Example code: XXXX-XXXX-XXXX-XXXX
+// Example site: https://supermariomakerbookmark.nintendo.net/courses/XXXX-XXXX-XXXX-XXXX
 
 class GetMarioMaker extends Command {
   constructor(msg) {
