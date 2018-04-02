8333243767d5cc45ce1858f6d9698913732be857
Alex Baratti <alexsmbaratti@gmail.com>
2018-04-01 21:17:18 -0700

    Adds more statistics

--- commands/Status.js
+++ commands/Status.js
@@ -12,20 +12,20 @@ class Status extends Command {
       // Pass in "this" from app.js
 
       var switchCodes;
-      MongoClient.connect(url, function(err, client) {
-        var db = client.db('bot');
+      MongoClient.connect(url, function(err, mongoClient) {
+        var db = mongoClient.db('bot');
         db.collection('users').count({
           switchCode: {
             $ne: "-1"
           }
         }, function(err, results) {
           switchCodes = results;
-          client.close();
+          mongoClient.close();
           let start = msg.createdTimestamp;
           msg.channel.send("```Status\n✅ Logged in as " + client.user.username + "!\n🔨 Build: " + build + "\n⏱ Ping: ...```")
             .then(message => {
               let diff = (message.createdTimestamp - start);
-              message.edit("```Status\n✅ Logged in as " + client.user.username + "!\n🔨 Build: " + build + "\n⏱ Ping: " + diff + "ms\n🔢  Nintendo Switch Codes: " + switchCodes + "```");
+              message.edit("```Status\n✅ Logged in as " + client.user.username + "!\n🔨 Build: " + build + "\n⏱ Ping: " + diff + "ms\n👥 Guilds Serving: " + client.guilds.size + "\n🔢  Nintendo Switch Codes: " + switchCodes + "```");
             })
             .catch(console.error);
         });
