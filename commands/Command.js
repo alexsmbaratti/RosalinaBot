b5ed8aae9cfac437738010f90d6724063123e121
Michael Leonffu <leonffu.michael@gmail.com>
2018-10-30 22:50:53 -0700

    Added commands tracking
    This will make ur database sad bc so much data. But it'll make u happy bc so much data. Also I should have probably tested it.... so its untested rn.
    
    Finally it should techinally have a if(err) but it doesn't. Although it shouldnt crash anything immediantly either.
    
    Once u have lots of data u can then arrgate to find useage and then make "data driven" choices

--- commands/Command.js
+++ commands/Command.js
@@ -1,6 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+
 class Command {
   constructor(msg) {
-
+  	 MongoClient.connect(url, function(err, client) {
+      var db = client.db('bot');
+      db.collection('commands').insertOne({
+        author: msg.author.id,
+        message: msg.content
+      }, function(err, results) {})
+    })
   }
 }
 
