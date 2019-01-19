1d1fa4ad0fc8735db94c5ad1c0a763acf4c8558c
Alex Baratti <alexsmbaratti@gmail.com>
2019-01-18 17:31:20 -0700

    Build 7.3.1 **January 18, 2019** * Adds r!ssbu command   * Share your Super Smash Bros. Ultimate Battle Arenas * Adds r!serverinfo command

---
+++ commands/ServerInfo.js
@@ -0,0 +1,75 @@
+const Command = require('../commands/Command.js');
+const color = 0x86D0CF;
+
+class ServerInfo extends Command {
+  constructor(msg) {
+    super(msg);
+    // Roles and IDs
+    // Channels
+    try {
+      var mGuild = msg.guild; 
+      if (mGuild.available) {
+        var bots = 0;
+        for (var i = 0; i < mGuild.members.array().length; i++) {
+          if (mGuild.members.array()[i].user.bot) {
+            bots++;
+          }
+        }
+        msg.channel.send({
+          embed: {
+            title: mGuild.name,
+            description: "Server Info",
+            color: color,
+            fields: [{
+                name: "Members",
+                value: mGuild.memberCount + " (" + bots + " bots)",
+                inline: true
+              },
+              {
+                name: "Region",
+                value: mGuild.region,
+                inline: true
+              },
+              {
+                name: "Owner",
+                value: mGuild.owner.user.tag,
+                inline: true
+              },
+              {
+                name: "Server ID",
+                value: mGuild.id,
+                inline: true
+              }
+            ],
+            thumbnail: {
+              url: mGuild.iconURL
+            },
+            footer: {
+              text: "Requested by " + msg.member.displayName,
+              icon_url: msg.author.avatarURL
+            }
+          }
+        });
+      } else {
+        msg.channel.send({
+          embed: {
+            description: "Server Unavailable",
+            color: color,
+            footer: {
+              text: "Please try again later"
+            }
+          }
+        });
+      }
+    } catch (e) {
+      msg.channel.send({
+        embed: {
+          description: "Cannot Get Server Info",
+          color: color
+        }
+      });
+    }
+  }
+}
+
+module.exports = ServerInfo;
