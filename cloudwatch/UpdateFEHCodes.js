ffe2c8ebe4be7e882e75dcd0f9e4051c910053c8
Alex Baratti <alexsmbaratti@gmail.com>
2019-08-07 15:25:54 -0700

    Adds FEH Logger Adds additional logs to app.js

--- cloudwatch/UpdateFEHCodes.js
+++ cloudwatch/UpdateFEHCodes.js
@@ -2,6 +2,7 @@ var AWS = require("aws-sdk");
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 AWS.config.loadFromPath('./config.json');
+const Logger = require('../utils/Logger.js');
 var cw = new AWS.CloudWatch({
   apiVersion: '2010-08-01'
 });
@@ -33,9 +34,9 @@ class UpdateFEHCodes {
 
         cw.putMetricData(params, function(err, data) {
           if (err) {
-            console.log("⛈  CloudWatch Put Error: ", err);
+            new Logger("\x1b[31mFailed to put FEH friend codes to CloudWatch!\x1b[0m");
           } else {
-            console.log("☁️  Put " + num + " FEH friend codes to CloudWatch");
+            new Logger(`\x1b[32mPut ` + num + ` FEH friend codes to CloudWatch\x1b[0m`);
           }
         });
       });
