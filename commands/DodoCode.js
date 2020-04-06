20d1eb41958cbc0f3fe8e5fd77f51d321138ec53
Alex Baratti <alexsmbaratti@gmail.com>
2020-04-06 00:08:17 -0700

    Sets up r!anch for island personalization

--- commands/DodoCode.js
+++ commands/DodoCode.js
@@ -1,4 +1,7 @@
 const Command = require('../commands/Command.js');
+const {RichEmbed} = require('discord.js');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
 const color = 0x86D0CF;
 
 // r!acnh DODOCODE
@@ -63,62 +66,57 @@ class DodoCode extends Command {
         if (arg1 == "") { // No arguments given
             msg.channel.send(":x: Invalid usage!\nCorrect usage is: `r!acnh [DODOCODE]`.")
         } else { // First argument given
+            let embed = new RichEmbed()
+                .setTitle("**Animal Crossing New Horizons**")
+                .setAuthor(name + " is inviting you to their island!", msg.author.avatarURL)
+                .setColor(color)
+                .setThumbnail("https://raw.githubusercontent.com/alexsmbaratti/RosalinaBot/indev/misc/ac_emblem_white.png")
+                .setFooter("Requires an active Nintendo Switch Online subscription to join")
             if (arg1.length == 5) { // Valid ID
-                if (arg2 == "NONE") {
-                    msg.channel.send({
-                        embed: {
-                            author: {
-                                name: name + " is inviting you to their island!",
-                                icon_url: msg.author.avatarURL
-                            },
-                            color: color,
-                            title: "**Animal Crossing New Horizons**",
-                            fields: [{
-                                name: "Dodo Code",
-                                value: arg1,
-                                inline: true
-                            }
-                            ],
-                            thumbnail: {
-                                url: "https://raw.githubusercontent.com/alexsmbaratti/RosalinaBot/indev/misc/ac_emblem_white.png"
-                            },
-                            footer: {
-                                text: "Requires an active Nintendo Switch Online subscription to join"
-                            }
-                        }
-                    }).then(message => {
-                        msg.delete();
-                    }).catch(console.error);
-                } else {
-                    msg.channel.send({
-                        embed: {
-                            author: {
-                                name: name + " is inviting you to their island!",
-                                icon_url: msg.author.avatarURL
-                            },
-                            color: color,
-                            title: "**Animal Crossing New Horizons**",
-                            fields: [{
-                                name: "Dodo Code",
-                                value: arg1,
-                                inline: true
-                            }, {
-                                name: "Native Fruit",
-                                value: arg2,
-                                inline: true
-                            }
-                            ],
-                            thumbnail: {
-                                url: "https://raw.githubusercontent.com/alexsmbaratti/RosalinaBot/indev/misc/ac_emblem_white.png"
-                            },
-                            footer: {
-                                text: "Requires an active Nintendo Switch Online subscription to join"
-                            }
-                        }
-                    }).then(message => {
-                        msg.delete();
-                    }).catch(console.error);
+                embed.addField("Dodo Code", arg1, true);
+                if (arg2 != "NONE") {
+                    embed.addField("Native Fruit", arg2, true);
                 }
+
+                MongoClient.connect(url, function (err, client) {
+                    var db = client.db('bot');
+                    db.collection('users').findOne({
+                        "_id": msg.author.id
+                    }, function (err, results) {
+                        if (results != null) {
+                            if (results.acnhIslandName != "-1") {
+                                embed.setAuthor(name + " is inviting you to " + results.acnhIslandName + "!", msg.author.avatarURL);
+                            }
+                            if (results.acnhFruit != "-1") {
+                                let fruit;
+                                switch (arg2.toLowerCase()) {
+                                    case "1":
+                                        fruit = "Pears 🍐";
+                                        break;
+                                    case "2":
+                                        fruit = "Apples 🍎";
+                                        break;
+                                    case "3":
+                                        fruit = "Peaches 🍑";
+                                        break;
+                                    case "4":
+                                        fruit = "Oranges 🍊";
+                                        break;
+                                    case "5":
+                                        fruit = "Cherries 🍒";
+                                        break;
+                                    default:
+                                        fruit = "ERROR";
+                                }
+                                embed.addField("Native Fruit", fruit, true);
+                            }
+                        }
+                        client.close();
+                    })
+                });
+
+
+                msg.channel.send(embed);
             } else { // ID not valid
                 msg.channel.send(":x: Dodo Code was invalid.")
             }
