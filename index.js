a32725ea734590bfc99e21d06570866ea25446a9
Alex Baratti <alexsmbaratti@gmail.com>
2021-01-29 21:48:48 -0700

    Updates Starbit and add Game.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const starbit = require('@alexsmbaratti/Starbit');
 const Discord = require('discord.js');
 const MongoDBDriver = require('@alexsmbaratti/Starbit/models/MongoDBDriver');
 const config = require('./config.json');
+const fs = require('fs');
 
 var driver = new MongoDBDriver(config.db.uri, config.db.dbName, true, config.db.userCollection);
 
 starbit.start(config.botToken, './bot/bot.js', {
     hostname: config.hostname,
     port: config.port,
-    keyPath: config.keyPath,
-    certificatePath: config.certificatePath,
+    httpsOpts: {key: fs.readFileSync(config.keyPath), cert: fs.readFileSync(config.certificatePath)},
     discordJSVersion: Discord.version,
     dbDriver: driver
 });
